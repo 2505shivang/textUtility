78b933d116baa84458e465042990ff1084797f1e
2505shivang <2505shivang@gmail.com>
2022-02-08 14:29:09 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@
 function App() {
   return (
     <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </nav>
-      <div className="container">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore cumque necessitatibus totam voluptatibus. Qui rem veniam officiis amet modi perspiciatis vero, magnam ullam error sunt officia numquam voluptas vel similique!
-        </p>
-      </div>
+        
     </>
   );
 }
